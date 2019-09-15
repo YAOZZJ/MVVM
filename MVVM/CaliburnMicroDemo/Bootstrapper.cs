cc1d16e38039e1fa7e49ff46b6a31aa7354dcd3c
DGYJ-X <DGYJ-X@DESKTOP-YAOZJ>
2019-09-15 09:23:16 +0800

    Caliburn.Micro

---
+++ MVVM/CaliburnMicroDemo/Bootstrapper.cs
@@ -0,0 +1,31 @@
+﻿using Caliburn.Micro;
+using CaliburnMicroDemo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CaliburnMicroDemo
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        #region Constructor
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        #endregion
+
+        #region Overrides
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //定义了一套简洁有效的 “命名约定” 功能，用于匹配 View 和 ViewModel 之间的联系。
+            //获取到指定 VM 的全名，并移除了 “Model” 部分，剩下的部分便是对应的 View
+            // DisplayRootViewFor<IShell>();
+            DisplayRootViewFor<WelcomeViewModel>();
+        }
+        #endregion
+    }
+}
---
+++ MVVM/CaliburnMicroDemo/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CaliburnMicroDemo.ViewModels
+{
+    public class WelcomeViewModel : PropertyChangedBase
+    {
+        public WelcomeViewModel()
+        {
+            _instruction = "YAOZHIJIAN";
+        }
+        private string _instruction;
+        /// <summary>
+        /// 对应View:x:Name="XXXX"
+        /// </summary>
+        public string TxtInstruction
+        {
+            get { return _instruction; }
+            set
+            {
+                _instruction = value;
+                NotifyOfPropertyChange(()=> TxtInstruction);
+                NotifyOfPropertyChange(() => CanBtnClickMe);
+            }
+        }
+        /// <summary>
+        /// 关联到方法BtnClickMe的可用性
+        /// </summary>
+        public bool CanBtnClickMe
+        {
+            get { return !string.IsNullOrWhiteSpace(TxtInstruction); }
+        }
+        /// <summary>
+        /// 对应VIew的Button :x:Name="XXXX"
+        /// </summary>
+        public void BtnClickMe()
+        {
+            MessageBox.Show($"Hello {TxtInstruction}");
+        }
+    }
+}
